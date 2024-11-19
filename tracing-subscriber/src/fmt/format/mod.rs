4cc8ebe3e6773ead683a5bd79b3b7dd1df3527bf
Madeleine Mortensen <maddie@ditto.com>
2024-11-19 13:50:42 +0000

    subscriber: change FormatTime to return the length of the formatted timestamp

--- tracing-subscriber/src/fmt/format/mod.rs
+++ tracing-subscriber/src/fmt/format/mod.rs
@@ -1752,8 +1752,10 @@ pub(super) mod test {
 
     pub(crate) struct MockTime;
     impl FormatTime for MockTime {
-        fn format_time(&self, w: &mut Writer<'_>) -> fmt::Result {
-            write!(w, "fake time")
+        fn format_time(&self, w: &mut Writer<'_>) -> Result<usize, fmt::Error> {
+            let time = "fake time";
+            write!(w, "{}", time)?;
+            Ok(time.len())
         }
     }
 
